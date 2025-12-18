9da7254523e5bf8d416fc320c5a00ad8c7473d8d
marssellina268-sketch <marssellina268@gmail.com>
2025-12-18 13:50:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".card button").forEach(button => {
+    button.addEventListener("click", () => {
+        alert("Fitur membaca buku akan segera tersedia 📖");
+    });
+});
